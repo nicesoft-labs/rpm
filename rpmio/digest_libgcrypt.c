207f486586779c6e68973e871cc88fa83a9e513e
nicesoft-labs <info@ncsgp.ru>
2025-07-01 22:55:31 +0300

    Update digest_libgcrypt.c

--- rpmio/digest_libgcrypt.c
+++ rpmio/digest_libgcrypt.c
@@ -654,9 +654,15 @@ pgpDigAlg pgpPubkeyNew(int algo, int curve, const char *oid)
         ka->mpis = 4;
         break;
     case PGPPUBKEYALGO_ECDSA:
+    if (ka->is_gost) {
+        ka->setmpi = pgpSetKeyMpiDSA; // или отдельный если нужен
+        ka->free = pgpFreeKeyDSA;
+        ka->mpis = 1;
+    } else {
         ka->setmpi = pgpSetKeyMpiDSA;
         ka->free = pgpFreeKeyDSA;
         ka->mpis = 4;
+    }
 	ka->curve = curve;
         break;
     case PGPPUBKEYALGO_GOST3410_2001:
